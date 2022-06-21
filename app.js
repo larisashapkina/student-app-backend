b3a2ef2ed2443716287ae5fd854fe9c91c834d21
Larisa <larisashapkinab@gmail.com>
2022-06-21 19:44:54 -0400

    add get all and get one on students controller

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const express = require('express');
 //initialize app
 const app = express();
 
+const studentsController = require('./controllers/studentsController');
+
+//we are going to tell app, hey whatever you get from students path, 
+//use the students controller
+app.use('/students', studentsController);
+
 //route
 app.get('/', (req, res)=>{
     res.send("Hello world!");
 })
+
+app.get('/')
+
 
 //export app
 module.exports=app;
