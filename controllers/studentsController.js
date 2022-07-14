789f58ed15b138716b7d4620d641abe1f3aca8d4
Larisa <larisashapkinab@gmail.com>
2022-07-13 23:27:38 -0400

    set up get request

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -20,18 +20,6 @@ controller.get('/', async (request, response) => {
 });
 
 
-// write a route to get a student by their full name
-
-// implement min and max ids for get students
-
-// write a route to get the grade average of a student by their id
-
-// get all students sorted by their last name
-
-
-// write a route that accepts a student id as part of the path
-// returning an object (JSON), representing the student with that id
-
 controller.get('/:id', async (request, response) => {
     try {
         const studentId = request.params.id;
@@ -52,6 +40,21 @@ controller.get('/:id', async (request, response) => {
     } catch (err){
         response.status(500).send("An error occurred");
     }
+})
+
+controller.get('/:id/grades', async(req, res)=>{
+try{
+    const studentId = req.params.id;
+
+    const grades = await db.any('SELECT * FROM grades WHERE student_id = $1', [studentId]);
+
+    grades.sort((a,b) => a.date - b.date);
+
+
+    res.json(grades);
+    }   catch(err) {
+        res.status(500).send(err);
+    }   
 })
 
 module.exports = controller;
