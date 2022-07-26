5780f4a1c83bedf865ff1fbbaaded15da01c882c
Larisa <larisashapkinab@gmail.com>
2022-07-26 12:37:46 -0400

    delete route

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -4,21 +4,34 @@ const controller = express.Router();
 const studentData = require('../studentData.json');
 
 const db = require('../db/index');
+const { response } = require('express');
 
 controller.get('/', async (request, response) => {
 
     let {limit=25, min, max} = request.query; 
 
     limit = Number(limit);
-
+    
     let studentDataForDelivery = await db.any('SELECT * FROM students');
-
+    
     studentDataForDelivery = studentDataForDelivery.slice(0, limit);
 
     response.json(studentDataForDelivery);
 
 });
 
+
+// write a route to get a student by their full name
+
+// implement min and max ids for get students
+
+// write a route to get the grade average of a student by their id
+
+// get all students sorted by their last name
+
+
+// write a route that accepts a student id as part of the path
+// returning an object (JSON), representing the student with that id
 
 controller.get('/:id', async (request, response) => {
     try {
@@ -29,7 +42,7 @@ controller.get('/:id', async (request, response) => {
             return;
         }
         
-        const singleStudent = await db.oneOrNone('SELECT * FROM students WHERE id=$1', [studentId]);
+        const singleStudent = await db.oneOrNone('SELECT * FROM students WHERE id = $1', [studentId]);
         
         if(singleStudent){
             response.json(singleStudent);
@@ -42,20 +55,21 @@ controller.get('/:id', async (request, response) => {
     }
 })
 
-controller.get('/:id/grades', async(req, res)=>{
-try{
-    const studentId = req.params.id;
+controller.get('/:id/grades', async (req, res) => {
 
-    const grades = await db.any('SELECT * FROM grades WHERE student_id = $1', [studentId]);
+    try {  
+        const studentId = req.params.id;
 
-    grades.sort((a,b) => a.date - b.date);
+        const grades = await db.any('SELECT * FROM grades WHERE student_id = $1', [studentId]);
 
+        grades.sort((a, b) => a.date - b.date);
 
-    res.json(grades);
-    }   catch(err) {
+        res.json(grades);
+        
+    } catch (err){
         res.status(500).send(err);
-    }   
-})
+    }
+});
 
 controller.delete('/:id', async (req, res) => {
     try {
